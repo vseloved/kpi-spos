9953ac6ef94bc2f6b31c3ec5e4fae509cff0771d
vseloved <vseloved@gmail.com>
2013-12-19 00:14:04 +0200

    Some rollback

--- heroku-compile.lisp
+++ heroku-compile.lisp
@@ -3,6 +3,7 @@
 (print ">>> Building system....")
 (require 'asdf)
 (require-quicklisp)
+(ql:quickload :hunchentoot)
 (print ">>> Done building system")
 
 ;; change the file just to invoke recompilation at heroku: 27
