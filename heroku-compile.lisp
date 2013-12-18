ee74c50f96adf79ccb8708e3a915af1f25aec980
vseloved <vseloved@gmail.com>
2013-12-19 00:04:18 +0200

    Another locale fix

--- heroku-compile.lisp
+++ heroku-compile.lisp
@@ -1,10 +1,6 @@
 (in-package :cl-user)
 
 (print ">>> Building system....")
-(require 'asdf)
-(asdf:disable-output-translations)
-(require-quicklisp)
-(ql:quickload :hunchentoot)
 (print ">>> Done building system")
 
 ;; change the file just to invoke recompilation at heroku: 27
